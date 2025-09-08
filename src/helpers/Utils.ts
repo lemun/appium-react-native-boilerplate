67ae71a65f78ec1722d4b7c6c977091dc34e4705
Shai Sinai <shaisinai4@gmail.com>
2025-09-08 12:13:13 +0300

    Adding getBy utility functions

--- src/helpers/Utils.ts
+++ src/helpers/Utils.ts
@@ -7,3 +7,35 @@ export function assertEnvironment(varName: string) {
   
   return envVarValue;
 };
+
+export function getByTestId(testId: string): ChainablePromiseElement {
+  return driver.isIOS ? $(`~${testId}`) : $(`android=new UiSelector().resourceId("${testId}")`);
+}
+
+export function getAllByTestId(testId: string): ChainablePromiseArray {
+  return driver.isIOS ? $$(`~${testId}`) : $$(`android=new UiSelector().resourceId("${testId}")`);
+}
+
+export function getByText(text: string): ChainablePromiseElement {
+  return driver.isIOS ? $(`-ios predicate string:name == "${text}"`) : $(`android=new UiSelector().text("${text}")`);
+}
+
+export function getByTextContains(text: string): ChainablePromiseElement {
+  return driver.isIOS
+    ? $(`-ios predicate string:name MATCHES "${text}"`)
+    : $(`android=new UiSelector().textContains("${text}")`);
+}
+
+export function getByTextMatches(text: string): ChainablePromiseElement {
+  return driver.isIOS
+    ? $(`-ios predicate string:name MATCHES "${text}"`)
+    : $(`android=new UiSelector().textMatches("${text}")`);
+}
+
+export function getByClass(className: string): ChainablePromiseElement {
+  return driver.isIOS ? $(`-ios class chain:${className}`) : $(`android=new UiSelector().className("${className}")`);
+}
+
+export function getByXpath(xpath: string): ChainablePromiseElement {
+  return driver.isIOS ? $(`-ios predicate string:xpath("${xpath}")`) : $(`android=new UiSelector().xpath("${xpath}")`);
+}
